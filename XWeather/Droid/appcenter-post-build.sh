8c400f794670a0ecdeeb5851604af86358a0b3a1
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 08:59:13 +0100

    Adding uitest tools dir

--- XWeather/Droid/appcenter-post-build.sh
+++ XWeather/Droid/appcenter-post-build.sh
@@ -2,4 +2,4 @@ echo "============================================="
 echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
  
-appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "$APPCENTER_OUTPUT_DIRECTORY/com.xamarin.xweather-Signed.apk" --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/UITests/bin/Debug" --token "$apiTokenXWeather" 
+appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "$APPCENTER_OUTPUT_DIRECTORY/com.xamarin.xweather-Signed.apk" --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/UITests/bin/Debug" --uitest-tools-dir "$APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools" --token "$apiTokenXWeather" 
