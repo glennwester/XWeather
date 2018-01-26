a0bcfbf081ebfbb4e6a4e67349945dffae6adade
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 07:01:28 +0100

    Removing token

--- XWeather/appcenter-post-build.sh
+++ XWeather/appcenter-post-build.sh
@@ -2,4 +2,4 @@ echo "============================================="
 echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
 
-appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "Droid/bin/Debug/com.xamarin.xweather-Signed.apk"  --test-series "master" --locale "en_US" --build-dir "UITests/bin/Debug" --token "7806248d469e63ef7b565b3718e33baf7d89ac30"
+appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "Droid/bin/Debug/com.xamarin.xweather-Signed.apk"  --test-series "master" --locale "en_US" --build-dir "UITests/bin/Debug"
