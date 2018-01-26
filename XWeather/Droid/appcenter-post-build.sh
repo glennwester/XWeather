003f68b52496cc3cca54242d4e1e676dc33d72be
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 07:23:03 +0100

    Changing post build script

--- XWeather/Droid/appcenter-post-build.sh
+++
@@ -1,5 +0,0 @@
-echo "============================================="
-echo "======= APP CENTER POSTBUILD SCRIPT ======="
-echo "============================================="
-
-appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "Droid/bin/Debug/com.xamarin.xweather-Signed.apk"  --test-series "master" --locale "en_US" --build-dir "UITests/bin/Debug" --token $apiTokenXWeather
