83b47486019b11da0714fd7e4cb5be68d2e350dc
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 15:59:32 +0100

    13

--- XWeather/Droid/appcenter-post-build.sh
+++ XWeather/Droid/appcenter-post-build.sh
@@ -6,28 +6,11 @@ echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
 
 echo "============================================="
-echo "base dir"
-cd /Users/vsts/agent/2.126.0/work/1/s/XWeather/UITests
+echo $APPCENTER_SOURCE_DIRECTORY/UITests
 ls
 echo "============================================="
-
-echo $APPCENTER_OUTPUT_DIRECTORY
-ls $APPCENTER_OUTPUT_DIRECTORY
-
-
-echo $APPCENTER_SOURCE_DIRECTORY/XWeather/UITests
-ls $APPCENTER_SOURCE_DIRECTORY/XWeather/UITests
-
-echo $APPCENTER_SOURCE_DIRECTORY
-ls $APPCENTER_SOURCE_DIRECTORY
-
-echo $APPCENTER_OUTPUT_DIRECTORY
-ls $APPCENTER_OUTPUT_DIRECTORY
-
-#ls $APPCENTER_OUTPUT_DIRECTORY
-#ls $APPCENTER_SOURCE_DIRECTORY/XWeather/Droid/bin/Debug
  
-appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "$APPCENTER_SOURCE_DIRECTORY/XWeather/Droid/bin/Debug/com.xamarin.xweather.apk" --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/XWeather/UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/XWeather/packages/Xamarin.UITest.2.2.2/tools --token $apiTokenXWeather 
+appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "$APPCENTER_SOURCE_DIRECTORY/XWeather/Droid/bin/Debug/com.xamarin.xweather.apk" --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.2.2.2/tools --token $apiTokenXWeather 
 
 
 #$APPCENTER_SOURCE_DIRECTORY/UITests/bin/Debug
