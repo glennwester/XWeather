2ede437fc2fc0c5a4772b6894d70b463a92d2bc5
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 14:09:39 +0100

    7

--- XWeather/Droid/appcenter-post-build.sh
+++ XWeather/Droid/appcenter-post-build.sh
@@ -5,7 +5,15 @@ echo "============================================="
 echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
 
+echo $APPCENTER_SOURCE_DIRECTORY/XWeather/UITests
 ls $APPCENTER_SOURCE_DIRECTORY/XWeather/UITests
+
+echo $APPCENTER_SOURCE_DIRECTORY
+ls $APPCENTER_SOURCE_DIRECTORY
+
+echo $APPCENTER_OUTPUT_DIRECTORY
+ls $APPCENTER_OUTPUT_DIRECTORY
+
 #ls $APPCENTER_OUTPUT_DIRECTORY
 #ls $APPCENTER_SOURCE_DIRECTORY/XWeather/Droid/bin/Debug
  
