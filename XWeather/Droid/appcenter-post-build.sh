350039a7cb38b15b80364494a5796b352e0261f9
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 11:57:06 +0100

    changing the script, again!

--- XWeather/Droid/appcenter-post-build.sh
+++ XWeather/Droid/appcenter-post-build.sh
@@ -4,6 +4,8 @@ npm install -g appcenter-cli
 echo "============================================="
 echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
+
+ls $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.2.2.2/tools
  
 appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path $APPCENTER_OUTPUT_DIRECTORY/com.xamarin.xweather-Signed.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_OUTPUT_DIRECTORY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.2.2.2/tools --token $apiTokenXWeather 
 
