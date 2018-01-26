c7101773912ce76fc3e24070cbeb7a4e4616937b
Glenn Wester <glennwester@users.noreply.github.com>
2018-01-26 07:09:16 +0100

    Replace token with private one!

--- XWeather/appcenter-post-build.sh
+++ XWeather/appcenter-post-build.sh
@@ -2,4 +2,4 @@ echo "============================================="
 echo "======= APP CENTER POSTBUILD SCRIPT ======="
 echo "============================================="
 
-appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "Droid/bin/Debug/com.xamarin.xweather-Signed.apk"  --test-series "master" --locale "en_US" --build-dir "UITests/bin/Debug"
+appcenter test run uitest --app "TechReady/XWeather" --devices 33cc6afe  --app-path "Droid/bin/Debug/com.xamarin.xweather-Signed.apk"  --test-series "master" --locale "en_US" --build-dir "UITests/bin/Debug" --token $apiTokenXWeather
